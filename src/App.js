9f8d6693780d0ba7bc1cd102c6cdd4735f8dbd38
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-11-05 22:54:14 +0100

    can track current position

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchBarComp from "./components/SearchBarCom";
 import CurrentWeatherComp from "./components/CurrentWeatherComp";
 import NextDaysComp from "./components/NextDaysComp";
 import icons from "./data/icons.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { useState } from "react";
 
 function App() {
@@ -12,6 +12,27 @@ function App() {
    const [lon, setLon] = useState(-0.1276474);
    const [lat, setLat] = useState(51.5073219);
    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+   // get user current location/position
+   useEffect(() => {
+      const options = {
+         enableHighAccuracy: true,
+         timeout: 5000,
+         maximumAge: 0,
+      };
+
+      function success(pos) {
+         const crd = pos.coords;
+         setLat(crd.latitude);
+         setLon(crd.longitude);
+      }
+
+      function error(err) {
+         console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+   }, []);
 
    // get the icon corresponding to the weather
    // icons are not perfect and sometimes are not completely relavant sometimes but I wanted to try and save little bit of time. Replacing the images in assets fix this issue
